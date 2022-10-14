0a3ddc6322e75ec40cd1d467fa9e49e368990558
cristianortiz25 <cristian_oc@corhuila.edu.co>
2022-10-13 22:33:31 -0500

    Ultima modificación actividadC2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">Registrar Ingreso</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align:center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 50px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.container {
+text-align: center;
+padding: 5px;
+color: #2c3e50;
+height:fit-content;
+display:block;
+}
+
+.registros{
+  width: 400px;
+  padding: 30px;  
+  background: #24303c;
+  margin: auto;
+  margin-top: 100px;
+  border-radius: 4px;
+  font-family: 'calibri';
+  color: white;
+  box-shadow: 7px 13px 37px #000;
+}
+
+.registros h4 {
+  font-size: 22px;
+  margin-bottom: 20px;
+}
+
+.controls{
+  width: 100%;
+  background: #f9f9fa;
+  padding: 10px;
+  border-radius: 4px;
+  margin-bottom: 16px;
+  border: 1px solid #aaaaf1;
+  font-family: 'calibri';
+  font-size: 18px;
+
+}
+
+.registros .botons{
+  width: 100%;
+  background: #aaaaf1;
+  border: none;
+  padding: 12px;
+  border-radius: 40px;
+  margin-bottom: 16px;
+  font-size: 16px;
 }
 </style>
